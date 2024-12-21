987cb323bfe01b157ff379a499161f8c853f9017
Quran2023 <ahlambedru9@gmail.com>
2024-12-21 12:52:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ def caesar(message, offset):
 
     for char in message.lower():
         if char == ' ':
-            decrypted_text += char
+            decrypted_text -= char
         else:
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
-            decrypted_text += alphabet[new_index]
+            decrypted_text -= alphabet[new_index]
 
     return decrypted_text
 
@@ -24,7 +24,7 @@ st.write("decrypt your text using the Caesar cipher.")
 text = st.text_input("Enter the text to be decrypted:")
 
 # Input for shift value
-shift = st.number_input("Enter the shift value (1-25):", min_value=1, max_value=25, value=3)
+shift = st.number_input("Enter the shift value (3-4):", min_value=1, max_value=25, value=3)
 
 # Button to decrypt
 if st.button("decrypt"):
@@ -35,38 +35,3 @@ if st.button("decrypt"):
     else:
         st.warning("Please enter a text to decrypt.")
 
-
-import streamlit as st
-
-def caesar(message, offset):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    decrypted_text = ''
-
-    for char in message.lower():
-        if char == ' ':
-            decrypted_text += char
-        else:
-            index = alphabet.find(char)
-            new_index = (index + offset) % len(alphabet)
-            decrypted_text += alphabet[new_index]
-
-    return decrypted_text
-
-# Streamlit app layout
-st.title("Caesar Cipher Web App")
-st.write("decrypt your text using the Caesar cipher.")
-
-# Input for plain text
-text = st.text_input("Enter the text to be decrypted:")
-
-# Input for shift value
-shift = st.number_input("Enter the shift value (1-25):", min_value=1, max_value=25, value=3)
-
-# Button to decrypt
-if st.button("decrypt"):
-    if text:
-        decrypted_text = caesar(text, shift)
-        st.write('**Plain text:**', text)
-        st.write('**decrypted text:**', decrypted_text)
-    else:
-        st.warning("Please enter a text to decrypt.")
