a4561a1b225db5a4d60106495fba16a209b7ad4a
Quran2023 <ahlambedru9@gmail.com>
2024-12-21 13:03:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,34 +4,33 @@ import streamlit as st
 
 def caesar(message, offset):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    decrypted_text = ''
+    encrypted_text = ''
 
     for char in message.lower():
         if char == ' ':
-            decrypted_text -= char
+            encrypted_text += char
         else:
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
-            decrypted_text -= alphabet[new_index]
+            encrypted_text += alphabet[new_index]
 
-    return decrypted_text
+    return encrypted_text
 
 # Streamlit app layout
 st.title("Caesar Cipher Web App")
-st.write("decrypt your text using the Caesar cipher.")
+st.write("Encrypt your text using the Caesar cipher.")
 
 # Input for plain text
-text = st.text_input("Enter the text to be decrypted:")
+text = st.text_input("Enter the text to be encrypted:")
 
 # Input for shift value
-shift = st.number_input("Enter the shift value (3-4):", min_value=1, max_value=25, value=3)
+shift = st.number_input("Enter the shift value (-25-25):", min_value=-25, max_value=25, value=4)
 
-# Button to decrypt
-if st.button("decrypt"):
+# Button to encrypt
+if st.button("Encrypt"):
     if text:
-        decrypted_text = caesar(text, shift)
+        encrypted_text = caesar(text, shift)
         st.write('**Plain text:**', text)
-        st.write('**decrypted text:**', decrypted_text)
+        st.write('**Encrypted text:**', encrypted_text)
     else:
-        st.warning("Please enter a text to decrypt.")
-
+        st.warning("Please enter a text to encrypt.")
